7ce9c430c9f74e2e96fb545373360cb97526f8ae
Aswin K <aswinkarat@gmail.com>
2022-05-28 23:07:30 +0530

    connecting to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+// setting up bodyParser and cors
+app.use(express.json({ limit: "30mb", extended: true}));
+app.use(express.urlencoded({ limit: '30mb', extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://asterdev:123123123@cluster0.zm24w57.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true })
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
 
