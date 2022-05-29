5cdc84b9a80a153d900e48ce614d05c002c734e6
Aswin K <aswinkarat@gmail.com>
2022-05-29 11:11:56 +0530

    updated controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,27 @@
-export const getPosts = (req, resp) => {
-    resp.send('THIS WORKS!!!');
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, resp) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        resp.status(200).json(postMessages);
+    } catch (error) {
+        resp.status(404).json({ message: error.message});
+    }
 }
+
+export const createPost = async (req,resp) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        resp.status(201).json(newPost);
+    } catch (error) {
+        resp.status(409).json({ message: error.message});
+    }
+}
+
+// https://www.restapitutorial.com/httpstatuscodes.html
