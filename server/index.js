2fe3318d6159d687afa25c15194b0b069e40ca4d
Aswin K <aswinkarat@gmail.com>
2022-05-30 20:14:43 +0530

    added JSX Structure

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "30mb", extended: true}));
 app.use(express.urlencoded({ limit: '30mb', extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://asterdev:123123123@cluster0.zm24w57.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://:@cluster0.zm24w57.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true })
